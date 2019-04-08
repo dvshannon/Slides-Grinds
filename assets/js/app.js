a911dc9859f59df8f9fb5d5ffe8cd65cad859239
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-04-08 13:51:10 -0400

    replaced seach function with scrollbar and made the site responsve across all platforms

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,39 @@ var tricks = [];
 var userInput;
 
 $('#add-trick').on('click', function(){
-  event.preventDefault();
-  $('.tricks-completed').empty();
-  userInput = $('#enter-trick').val().trim();
-  tricks.push(userInput);
-    console.log(tricks);
+	event.preventDefault();
+	$('.tricks-completed').empty();
 
-    for (var i = 0; i < tricks.length; i++) {
-      var landed = $('<div>').addClass('landedBtn');
-      landed.text(tricks[i]);
-      $('.tricks-completed').prepend(landed);
-      $('#enter-trick').text('');
-    }
+	var side = $('#side').val();
+	var pop = $('#pop').val();
+	var turn = $('#turn').val();
+	var flip = $('#flip').val();
+	var grinds = $('#grinds').val();
+	var to = $('#to').val();
+	var grinds2 = $('#grinds-2').val();
+
+	landed = side + ' ' + pop + ' ' + turn + ' ' + flip + ' ' + grinds + ' ' + to + ' ' + grinds2;
+
+	tricks.push(landed);
+
+	for (var i = 0; i < tricks.length; i++) {
+		var landed = $('<div>').addClass('landedBtn');
+		landed.attr("data-type", "land");
+		landed.text(tricks[i]);
+		$('.tricks-completed').prepend(landed);
+		$('#enter-trick').text('');
+	
+	
+		$(document).on('click', '.landedBtn', function(){
+			var state = $(this).attr('data-type');
+
+			if (state === 'land') {
+				$(this).attr("data-type", "bail");
+			} else {
+				$(this).attr("data-type", "land");
+			}
+		});
+	}
+	console.log(tricks);
 })
+
